fd0cc14b7ea1dd71a34b99c04bda89eea082410f
Diego Sarmanho <68619825+sarmanhodev@users.noreply.github.com>
2025-12-09 15:58:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from functions import *
+from models import *
 
 app = FastAPI()
 
-
-
-class Item(BaseModel):
-    texto: str
-
+# 🔹 Configurar CORS para permitir requisições de domínios externos
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os headers
+)
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
@@ -19,5 +24,27 @@ async def root():
 
 @app.post("/resumir/")
 async def resumir_texto(item: Item):
-    resultado = await resume_text(item.texto)
-    return {"resumo": resultado}
+    try:
+        resultado = await resume_text(item.texto)
+        return {"resumo": resultado}
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={"erro": str(e)}
+        )
+
+
+@app.post("/gerar_query")
+async def gerar_query_sql(dados: PerguntaRequest):
+    pergunta_usuario = dados.pergunta
+
+    try:
+        resposta_en = await converter_texto_en(pergunta_usuario)
+
+        resposta_query = get_sql(resposta_en)
+
+        return {"resposta_sql": resposta_query}
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"erro": str(e)})
